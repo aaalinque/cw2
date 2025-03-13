0a5c6e869bc593a2aca9486d728261e6911064d4
aaalinque <s33028@pjwstk.edu.pl>
2025-03-13 10:53:05 +0100

    spoko

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
-    public static void main (String[] args){
+    public static int[] filterEvenIndexOddValue(int[] array) {
 
+        return new int[0];
     }
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5, 6};
+        int[] array2 = {2, 3, 4, 5, 6, 7};
+        int[] array3 = {7, 8, 9, 10, 11};
 
+        printArray(filterEvenIndexOddValue(array1));
+        printArray(filterEvenIndexOddValue(array2));
+        printArray(filterEvenIndexOddValue(array3));
+    }
 }
